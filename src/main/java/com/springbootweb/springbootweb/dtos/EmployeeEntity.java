e4d83fa0d4ca466d5686dc4818ca9c72dbf691f2
Nitu <nitu.kumari@sisindia.com>
2026-01-29 21:29:33 +0530

    service layer,writing your business logic

--- src/main/java/com/springbootweb/springbootweb/dtos/EmployeeEntity.java
+++ src/main/java/com/springbootweb/springbootweb/dtos/EmployeeEntity.java
@@ -1,11 +1,19 @@
 package com.springbootweb.springbootweb.dtos;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name="employees")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
